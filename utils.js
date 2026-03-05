bf91e5e112eb5d1e46fcdc16b6e192cf8fe60631
Valeriaaxbt <133218023+yusufsamet522@users.noreply.github.com>
2026-03-05 21:44:05 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:43:28.224Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-05T18:44:03.539Z
+Commit: Add export functionality for reports
