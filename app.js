f80846605635a5735950a8102481d595717bcdd8
Valeriaaxbt <133218023+yusufsamet522@users.noreply.github.com>
2026-03-05 21:44:01 +0300

    Patch API endpoint error handling

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:43:49.001Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T18:43:59.690Z
+Commit: Patch API endpoint error handling
