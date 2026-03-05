c69ef632fdcdbe8edd19176f6f9eecdf7ca24d4c
Valeriaaxbt <133218023+yusufsamet522@users.noreply.github.com>
2026-03-05 21:44:15 +0300

    Patch API endpoint error handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:43:32.374Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-05T18:44:13.930Z
+Commit: Patch API endpoint error handling
