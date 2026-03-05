d56d8f9874205bf583a524fdaf10b25e08efe1f3
Valeriaaxbt <133218023+yusufsamet522@users.noreply.github.com>
2026-03-05 21:45:34 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:45:18.327Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-05T18:45:32.861Z
+Commit: Fix broken links in documentation
