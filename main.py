9a7501d5bad8065efcd2e3ac8be65e18197f25f6
Valeriaaxbt <133218023+yusufsamet522@users.noreply.github.com>
2026-03-05 21:45:24 +0300

    Fix CSS styling bug on mobile devices

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:45:07.931Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-05T18:45:22.476Z
+Commit: Fix CSS styling bug on mobile devices
