3c9ef5cba9aca31ace6321544a716946858b5dea
Valeriaaxbt <133218023+yusufsamet522@users.noreply.github.com>
2026-03-05 21:44:07 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:44:03.539Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T18:44:05.617Z
+Commit: Patch API endpoint error handling
